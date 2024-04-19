0c178b25320a361fdcc6da86fbe47398ba78e6a4
KanivetsEM <115340690+KanivetsEM@users.noreply.github.com>
2024-04-19 19:23:42 +0300

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://7b6b5ade-dbda-4523-aad6-50978727d2a9.serverhub.praktikum-services.ru"
+URL_SERVICE = "https://533deef9-ee05-43bd-a120-af58e9edd2c9.serverhub.praktikum-services.ru"
 CREATE_ORDER_PATH = "/api/v1/orders"
 ORDER_TRACK_PATH = "/api/v1/orders/track?t="
